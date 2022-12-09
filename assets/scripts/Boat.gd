91a27086d7e052321ffbec3fa3d2d452d66447b2
joshiiwoshi <joshiiwoshi@gmail.com>
2022-12-09 15:05:24 -0800

    add player movement

--- assets/scripts/Boat.gd
+++ assets/scripts/Boat.gd
@@ -1,2 +1,22 @@
 extends KinematicBody2D
 
+export (int) var speed = 200
+export (float) var rotation_speed = 1.5
+
+var velocity = Vector2.ZERO
+var rotation_dir = 0
+
+func get_input() -> void:
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed("row_left"):
+		rotation_dir += 1
+		velocity = Vector2(-speed, 1).rotated(rotation)
+	if Input.is_action_pressed("row_right"):
+		rotation_dir -= 1
+		velocity = Vector2(speed, 1).rotated(rotation)
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
