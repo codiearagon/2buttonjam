633da7367203df2ccaac9a7a01f323314b7e5123
otoxys <justinmatthewrojas@gmail.com>
2022-12-11 03:11:00 +0800

    added an oar and cleaned up my InvertRotation code
    well its not my code it's actually joshiwoshii's but you get the point

--- assets/scripts/InvertRotation.gd
+++ assets/scripts/InvertRotation.gd
@@ -1,32 +1,18 @@
 extends AnimatedSprite
 
-export (int) var maxSpeed = 50
-export (float) var rotation_speed = 0.03
-export var sprite_scale = Vector2(1.75, 1.75)
-#export (float) var decay = 5
-
-var velocity = Vector2.ZERO
+var rotation_speed = 0.03
 var rotation_dir = 0
-var speed = 0
 var isFlipped = 0
 
 func get_input() -> void:
 	if Input.is_action_pressed("row_left"):
 		rotation_dir -= 1
-		speed = maxSpeed
 		isFlipped = 1
 	if Input.is_action_pressed("row_right"):
 		rotation_dir += 1
-		speed = maxSpeed
 		isFlipped = 0
-	velocity = Vector2(0, -speed).rotated(rotation)
 
 func _physics_process(delta: float) -> void:
 	get_input()
-	#flips the sprite (happens when an input happens)
 	set_flip_h(isFlipped)
-
 	rotation = lerp_angle(rotation, rotation_dir * rotation_speed, delta)
-	
-	speed = lerp(speed, 0, 0.05)
-	speed = clamp(speed, 0, maxSpeed)
--- assets/scripts/Oar.gd
+++ assets/scripts/Oar.gd
@@ -1,26 +1,15 @@
 extends Sprite
 
-export (int) var maxSpeed = 50
-export (float) var rotation_speed = 0.03
-
-var velocity = Vector2.ZERO
+export (float, 0, 1) var rotation_speed = 0.3
 var rotation_dir = 0
-var speed = 0
+var side = 0
 
 func get_input() -> void:
 	if Input.is_action_pressed("row_left"):
-		rotation_dir += 1
-		speed = maxSpeed
+		side = 0
 	if Input.is_action_pressed("row_right"):
-		rotation_dir -= 1
-		speed = maxSpeed
-		
-	velocity = Vector2(0, -speed).rotated(rotation)
-	
+		side = 1
 
 func _physics_process(delta: float) -> void:
 	get_input()
-	rotation = lerp_angle(rotation, rotation_dir * rotation_speed, delta)
-	
-	speed = lerp(speed, 0, 0.05)
-	speed = clamp(speed, 0, maxSpeed)
+	rotation = lerp_angle(rotation, side * PI , rotation_speed)
