5e4901a1ff4cc5c3bce70808a549bcb40953d06e
joshiiwoshi <joshiiwoshi@gmail.com>
2022-12-10 08:58:15 -0800

    update boat movement

--- assets/scripts/Boat.gd
+++ assets/scripts/Boat.gd
@@ -1,15 +1,14 @@
 extends KinematicBody2D
 
 export (int) var maxSpeed = 50
-export (float) var rotation_speed = 1
-export (float) var decay = 5
+export (float) var rotation_speed = 0.03
+#export (float) var decay = 5
 
 var velocity = Vector2.ZERO
 var rotation_dir = 0
 var speed = 0
 
 func get_input() -> void:
-	rotation_dir = 0
 	if Input.is_action_pressed("row_left"):
 		rotation_dir += 1
 		speed = maxSpeed
@@ -22,7 +21,8 @@ func get_input() -> void:
 
 func _physics_process(delta: float) -> void:
 	get_input()
-	rotation += lerp_angle(rotation, rotation_dir * rotation_speed * delta, 1)
+	rotation = lerp_angle(rotation, rotation_dir * rotation_speed, delta)
+	print(rotation)
 	velocity = move_and_slide(velocity)
 	
 	speed = lerp(speed, 0, 0.05)
