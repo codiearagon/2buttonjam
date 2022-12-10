d67795118ac9afc91664faec70ebfc179050339d
otoxys <justinmatthewrojas@gmail.com>
2022-12-11 01:03:57 +0800

    animated sprite spinning
    holy this is so cool :OOO :100:

---
+++ assets/scripts/InvertRotation.gd
@@ -0,0 +1,28 @@
+extends AnimatedSprite
+
+export (int) var maxSpeed = 50
+export (float) var rotation_speed = 0.03
+#export (float) var decay = 5
+
+var velocity = Vector2.ZERO
+var rotation_dir = 0
+var speed = 0
+
+func get_input() -> void:
+	if Input.is_action_pressed("row_left"):
+		rotation_dir -= 1
+		speed = maxSpeed
+	if Input.is_action_pressed("row_right"):
+		rotation_dir += 1
+		speed = maxSpeed
+		
+	velocity = Vector2(0, -speed).rotated(rotation)
+	
+
+func _physics_process(delta: float) -> void:
+	get_input()
+	rotation = lerp_angle(rotation, rotation_dir * rotation_speed, delta)
+	print(rotation)
+	
+	speed = lerp(speed, 0, 0.05)
+	speed = clamp(speed, 0, maxSpeed)
