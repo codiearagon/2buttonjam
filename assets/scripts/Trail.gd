48d1c11ab9ec83b1e7748fb27288a93fb8cd0c8e
joshiiwoshi <joshiiwoshi@gmail.com>
2022-12-10 16:51:03 -0800

    fix trail and collision shape

---
+++ assets/scripts/Trail.gd
@@ -0,0 +1,27 @@
+extends Line2D
+
+export (int, 0, 100) var length = 40
+var child_position = Vector2.ZERO
+var parent_position = Vector2.ZERO
+var parent_rotation = 0
+var framelimiter = 0
+
+func _process(delta):
+	parent_rotation = get_parent().rotation
+	parent_position = get_parent().position
+	
+	global_position = Vector2(0, -30).rotated(parent_rotation)
+	global_rotation = 0
+	
+	framelimiter += delta
+	
+	if framelimiter > 1.0/24:
+		child_position = parent_position
+		add_point(parent_position)
+		while get_point_count() > length:
+			remove_point(0)
+		
+		framelimiter = 0
+		
+	#print("Parent Pos: ", parent_position.x, ", ", parent_position.y)
+	#print("Child Pos: ", child_position.x, ", ", child_position.y)
