3754b8ba63abd6cf5fe53dbd0d28c8bb1c2720e5
joshiiwoshi <joshiiwoshi@gmail.com>
2022-12-14 12:33:12 -0800

    add animation for level 2 start

--- assets/UI/Timer.gd
+++ assets/UI/Timer.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-export var max_seconds = 119
+export var max_seconds: int = 120
 
 func _ready():
 	$Survive_Timer.wait_time = max_seconds
 	$Survive_Timer.start()
 	
 func _process(delta: float) -> void:
-	var minutes = ceil($Survive_Timer.time_left / 60)
-	var seconds = floor($Survive_Timer.time_left / minutes)
+	var minutes = floor($Survive_Timer.time_left / 60)
+	var seconds = floor($Survive_Timer.time_left / (minutes + 1))
 	
 	$GUI/Timer/Panel/Label.text = str(minutes) + ":" + str(seconds)
 
