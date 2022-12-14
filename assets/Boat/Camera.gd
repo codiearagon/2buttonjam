4ba14a1d1090962d4ab3c2a2eeb6a88a669426a7
joshiiwoshi <joshiiwoshi@gmail.com>
2022-12-14 11:20:06 -0800

    add fade effect to change scene

--- assets/Boat/Camera.gd
+++
@@ -1,4 +0,0 @@
-extends Camera2D
-
-func _process(_delta: float) -> void:
-	position = get_node("../Boat").position
--- assets/Gate/Goal_Area.gd
+++ assets/Gate/Goal_Area.gd
@@ -3,4 +3,4 @@ extends Area2D
 #var next_level = preload("res://assets/Scenes/Level_2.tscn")
 
 func _on_Goal_Area_body_entered(body: Node) -> void:
-	get_tree().change_scene("res://assets/Scenes/Level_2.tscn")
+	SceneTransition.change_scene("res://assets/Scenes/Level_2.tscn")
