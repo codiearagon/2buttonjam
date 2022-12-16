5b5b021305fd80e79fd61dd5c02ff70090caec5a
otoxys <justinmatthewrojas@gmail.com>
2022-12-17 06:51:14 +0800

    level 2
    okay its basically just a lake but

--- assets/Boat/Boat_Scripts/PlayerBullet.gd
+++ assets/Boat/Boat_Scripts/PlayerBullet.gd
@@ -4,12 +4,33 @@ var bullet_speed = 0
 var bullet_damage = 0
 var boat_position = Vector2.ZERO
 var boat_rotation = 0
+var phantom_position = Vector2.ZERO
+var phantom_direction = 0
 var direction = Vector2.ZERO
 
 func _ready():
 	boat_position = get_parent().get_node("./Boat").position
 	boat_rotation = get_parent().get_node("./Boat").rotation
-	direction = Vector2(0, 1).rotated(boat_rotation)
+	direction.x = cos(boat_position.x - phantom_position.x)
+	direction.y = sin(boat_position.y - phantom_position.y)
+
+#	direction = Vector2(0, 1).rotated(boat_rotation)
+
+func get_closest_enemy():
+	var enemies = get_tree().get_nodes_in_group('Phantom')
+	if enemies.empty(): return null
+
+	var distances = []
+
+	for enemy in enemies:
+		var distance = get_parent().get_node("Boat").global_position.distance_squared_to(enemy.global_position)
+		distances.append(distance)
+
+	var min_distance = distances.min()
+	var min_index = distances.find(min_distance)
+	var closest_enemy = enemies[min_index]
+
+	return closest_enemy
 	
 func _physics_process(delta: float) -> void: 
 	move_and_slide(direction * bullet_speed)
