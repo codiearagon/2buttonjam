0bf79bbcb93ae633c50a499e0f1688eba50e9ce7
joshiiwoshi <joshiiwoshi@gmail.com>
2022-12-15 22:27:16 -0800

    add script to boss

--- assets/Enemy/Boss/Boss.gd
+++ assets/Enemy/Boss/Boss.gd
@@ -1,2 +1,23 @@
 extends "../EnemyBase.gd"
 
+
+func _ready():
+	max_health = 50
+	movement_speed = 100
+	bullet_damage = 10
+	bullet_speed = 50
+	attack_rate = 2
+	
+	health = max_health
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func first_phase_change():
+	pass
+
+func second_phase_change():
+	pass
+
+func third_phase_change():
+	pass
